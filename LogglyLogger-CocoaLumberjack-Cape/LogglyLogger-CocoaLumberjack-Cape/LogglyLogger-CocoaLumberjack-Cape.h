7cb48130e322b5f256269e7ca2cc232377ab87b3
jd <jd@cape.com>
2017-07-11 14:31:40 -0700

    adds framework + project + carthage

---
+++ LogglyLogger-CocoaLumberjack-Cape/LogglyLogger-CocoaLumberjack-Cape/LogglyLogger-CocoaLumberjack-Cape.h
@@ -0,0 +1,19 @@
+//
+//  LogglyLogger-CocoaLumberjack-Cape.h
+//  LogglyLogger-CocoaLumberjack-Cape
+//
+//  Created by JD Stockford on 7/11/17.
+//  Copyright © 2017 JD Stockford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LogglyLogger-CocoaLumberjack-Cape.
+FOUNDATION_EXPORT double LogglyLogger_CocoaLumberjack_CapeVersionNumber;
+
+//! Project version string for LogglyLogger-CocoaLumberjack-Cape.
+FOUNDATION_EXPORT const unsigned char LogglyLogger_CocoaLumberjack_CapeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LogglyLogger_CocoaLumberjack_Cape/PublicHeader.h>
+
+
